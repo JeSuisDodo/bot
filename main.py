58be9e9dd9fb54bb7fa859c4679af60ff71bfe39
Dodo <doriand.alessandrini@gmail.com>
2023-01-04 20:35:33 +0100

    continue

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import os
 import discord 
+from discord.ext import commands
+from asyncio import sleep
+import os
+from dotenv import load_dotenv
 
-bot = discord.Bot()
 
-guild_ids = [int(os.getenv(834429780916830280))]
+load_dotenv()
 
-@bot.event
-async def conect():
-    print(f"Connecter sous le nom de {bot.user}")
 
-@bot.slash_command()
-async def test(ctx):
-    await ctx.respond("Yep !")
 
-@bot.command(name='membercount')
-async def membercount(ctx):
-    await ctx.send(len(discord.guild_ids.member_count))
+intents = discord.Intents.default()
+intents.members = True
+client = commands.Bot(command_prefix='!', help_command=None, intents=intents)
 
-bot.run("MTA1OTUxNDY2NzgyOTAzNTAxOA.Gu86KI.7p20hi4sFqYT7eopHj_GcLv_3tzgwNa2tG-ajc")
+@client.event()
+async def on_ready():
+	await client.wait_until_ready()
+	print('online')
+	
+@client.event
+async def on_member_join(member):
+	await sleep(60*10)
+	for channel in member.guild.channels: 
+		if channel.name.startswith('Member'):
+			await channel.edit(name=f'Members: {member.guild.member_count}')
+			break
 
+
+client.run(os.getenv("MTA1OTUxNDY2NzgyOTAzNTAxOA.Gu86KI.7p20hi4sFqYT7eopHj_GcLv_3tzgwNa2tG-ajc"))
